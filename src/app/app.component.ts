1310b72202ff3dbb51c51b7b9f2947795aa9848f
karandixit10 <karandixit10@gmail.com>
2020-07-30 17:32:28 +0530

    newCrud

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,79 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DynamicGrid } from './grid.model';
+import { FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'sample';
+export class AppComponent  implements OnInit  {
+
+  dynamicArray: Array<DynamicGrid> = [];
+  newDynamic: any = {};
+
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+    this.newDynamic = { Name: '',
+    Qnt: '',
+    Rate: '',
+    Value: '',
+    Discount: '',
+    Discounted_Price: '',
+    Tax: '',
+    Tax_value: '',
+    Total: '',
+  };
+    this.dynamicArray.push(this.newDynamic);
+}
+
+
+addRow() {
+  this.newDynamic = { Name: '',
+    Qnt: '',
+    Rate: '',
+    Value: '',
+    Discount: '',
+    Discounted_Price: '',
+    Tax: '',
+    Tax_value: '',
+    Total: '',
+  };
+  this.dynamicArray.push(this.newDynamic);
+  // return true;
+  console.log(this.dynamicArray);
+}
+onDelete(index) {
+
+    this.dynamicArray.splice(index, 1);
+    return true;
+
+}
+totalValue() {
+  var total = this.newDynamic.Qnt * this.newDynamic.Rate;
+  this.newDynamic.Value = total;
+  return total ;
+}
+
+discountedValue(){
+  var disc = this.totalValue() - ((this.totalValue()) * (this.newDynamic.Discount) * 0.01 ) ;
+  this.newDynamic.Discounted_Price = disc;
+  return disc;
+}
+
+tax(){
+   var taxPrice = this.discountedValue() * this.newDynamic.Tax * 0.01 ;
+   this.newDynamic.Tax_value = taxPrice;
+   return taxPrice;
+}
+grandTotal(){
+   var grandT =  this.discountedValue() + this.tax();
+   this.newDynamic.Total = grandT;
+   return grandT;
+}
+
 }
---
+++ src/app/grid.model.ts
@@ -0,0 +1,11 @@
+export class DynamicGrid{
+  Name: string;
+  Qnt: number;
+  Rate: number;
+  Value: number;
+  Discount: number;
+  Discounted_Price: number;
+  Tax: number;
+  Tax_Value: number;
+  Total: number;
+}
